af4e69238c82706bf4bc0de33acfc5623e97e4dd
Raafat A. Eissa <76243557+RaafatA@users.noreply.github.com>
2025-04-19 08:43:32 +0200

    Update miRDeep2.sh

--- miRNA-seq/miRDeep2.sh
+++ miRNA-seq/miRDeep2.sh
@@ -76,14 +76,15 @@ grep -A1 '^>hsa-' hairpin.fa > hsa.hairpin.fa
 #   -s : output file for collapsed reads
 #   -t : output mapping results in .arf format (needed by miRDeep2.pl)
 #   -p : prefix of the Bowtie index built earlier
-mapper.pl SRR1234567_trimmed.fastq -e -h -m -j -l 18 -s reads_collapsed.fa -t reads_vs_genome.arf -p hg38_index
+mapper.pl SRR1234567_trimmed.fastq -e -d -h -m -j -l 18 -s reads_collapsed.fa -t reads_vs_genome.arf -p hg38_index
 
 #############################
 # 7. Run miRDeep2 for miRNA quantification and discovery
 #############################
 # miRDeep2.pl takes the collapsed reads, the genome, mapping file, and the miRBase references.
 # Note: the second parameter for mature sequences is repeated – this allows using the same file for both known and candidate quantification.
-miRDeep2.pl reads_collapsed.fa Homo_sapiens.GRCh38.dna.primary_assembly.fa reads_vs_genome.arf hsa.mature.fa hsa.hairpin.fa -t hsa
+awk 'print $1' Homo_sapiens.GRCh38.dna.primary_assembly.fa > one_column_ref.fa
+miRDeep2.pl reads_collapsed.fa one_column_ref.fa reads_vs_genome.arf hsa.mature.fa hsa.hairpin.fa -t hsa
 
 # The output directory will contain files including:
 #   - HTML reports with predicted miRNAs and their scores
